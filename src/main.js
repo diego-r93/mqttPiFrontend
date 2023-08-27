99b4c3e1486f3e2b4091131e88fdd8586f0026e2
diego <diegorodrigues-5@hotmail.com>
2023-08-27 02:22:45 -0300

    :sparkles: Configurando rotas para o backend

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
 
 // Components
 import App from './App.vue'
+import router from './router'
 
 // Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+const pinia = createPinia()
 const app = createApp(App)
+app.use(pinia)
 
 registerPlugins(app)
 
-app.mount('#app')
+app.use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,104 @@
-// Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import Authentication from '@/services/auth'
 
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
+    redirect: "/dashboard",
+    meta: {},
+  },
+  {
+    path: "/dashboard",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/DashBoard.vue"),
+  },
+  {
+    path: "/device",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/DeviceConfiguration.vue"),
+  },
+  {
+    path: "/database",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/DatabaseConfiguration.vue"),
+  },
+  {
+    path: "/account",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/MyAccount.vue"),
+  },
+  {
+    path: "/configuration",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/PageConfiguration.vue"),
+  },
+  {
+    path: "/help",
+    meta: {
+      requiresAuth: true,
+      layout: "ui",
+    },
+    component: () => import("@/views/HelpMenu.vue"),
+  },
+  {
+    path: '/login',
+    meta: {
+      layout: "auth",
+    },
+    component: () => import("@/views/LoginPage.vue"),
+  },
+  {
+    path: '/signup',
+    meta: {
+      layout: "auth",
+    },
+    component: () => import("@/views/SignUpPage.vue"),
+  },
+  {
+    path: '/resetpassword',
+    meta: {
+      layout: "auth",
+    },
+    component: () => import("@/views/SendPasswordResetEmail.vue"),
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+})
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  const authenticated = Authentication.isAuthenticated()
+
+  if (requiresAuth && !authenticated) {
+    next('/login')
+  }
+  else next()
 })
 
 export default router
